74de80a873041f1c842ea41a2d4d95f1121cf610
José Luis Olivares <joseluis_503@yahoo.com>
2025-07-14 11:18:53 -0600

    [Add]  Crando archivo de configuración tsconfig.json

---
+++ 01-Intro-TypeScript/02-ts-config/my-typescript.js
@@ -0,0 +1,9 @@
+"use strict";
+let nombre = "Escriba su nombre";
+let apellidos = "Escriba sus apellidos";
+console.log(`Hola, ${nombre} ${apellidos} Bienvenido a TypeScript!`);
+//Este es un comentario de una sola línea
+/*
+Este es un comentario de varias líneas
+Puedes escribir cuanquier cantidad de lineas
+hasta que llegues al cierre del bloque */ 
