718e2f48bab1285d40a0d48b65aef60ab51b6711
José Luis Olivares <joseluis_503@yahoo.com>
2025-07-15 17:18:16 -0600

    [Add] Ejemplo uso de type

---
+++ 01-Intro-TypeScript/04-types/01-types-ejemplo.ts
@@ -0,0 +1,16 @@
+//Declaración
+type ID = string;
+type Coordenada = { x: number; y: number };
+type Estado = 'activo' | 'inactivo' | 'pendiente'; // Definición de un tipo de unión 
+
+
+//Uso 
+let miID: ID = '123';
+let miCoordenada: Coordenada = { x: 10, y: 20 };
+let miEstado: Estado = 'activo';
+
+
+
+
+
+
