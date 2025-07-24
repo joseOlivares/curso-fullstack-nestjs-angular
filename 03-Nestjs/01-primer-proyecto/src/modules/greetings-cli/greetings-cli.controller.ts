b91a6e7844646de27f7973854e1b23084b9a0984
José Luis Olivares <joseluis_503@yahoo.com>
2025-07-23 21:35:08 -0600

    [Add] se implementa lógica

--- 03-Nestjs/01-primer-proyecto/src/modules/greetings-cli/greetings-cli.controller.ts
+++ 03-Nestjs/01-primer-proyecto/src/modules/greetings-cli/greetings-cli.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { GreetingsCliService } from './greetings-cli.service';
 
-@Controller('greetings-cli')
-export class GreetingsCliController {}
+@Controller('greetings-cli') // http://localhost:3000/greetings-cli
+export class GreetingsCliController {
+  //inyectamos el servicio
+  constructor(private greetingsService: GreetingsCliService) {}
+  @Get()
+  getGretting(): string {
+    return this.greetingsService.sayHello();
+  }
+
+  @Post()
+  postGretting(@Body() body: { name: string }): string {
+    return this.greetingsService.sayHelloWithName(body.name);
+  }
+
+  //con multiples parámetros en el body
+  @Post('/full-name') // http://localhost:3000/greetings-cli/full-name
+  postGretting2(@Body() body: { name: string; sureName?: string }): string {
+    return this.greetingsService.sayHelloWithFullName(body.name, body.sureName);
+  }
+}
