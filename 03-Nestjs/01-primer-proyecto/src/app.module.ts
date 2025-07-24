5b50caf01b524b40d0bbffb26dd0a32bb114bac1
José Luis Olivares <joseluis_503@yahoo.com>
2025-07-23 21:31:07 -0600

    [Add] Creando modulo, controler y servicio con la CLI

--- 03-Nestjs/01-primer-proyecto/src/app.module.ts
+++ 03-Nestjs/01-primer-proyecto/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GreetingsModule } from './modules/greetings/greetings.module';
+import { GreetingsCliModule } from './modules/greetings-cli/greetings-cli.module';
 
 @Module({
-  imports: [GreetingsModule],
+  imports: [GreetingsModule, GreetingsCliModule],
   controllers: [AppController],
   providers: [AppService],
 })
